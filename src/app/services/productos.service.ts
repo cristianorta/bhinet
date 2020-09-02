d364d888b5524763d7da885d9abdf4a080de923f
Cristian <cristian@bhinet.com.ar>
2020-09-02 09:20:12 -0300

    Fin de la seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,6 +10,7 @@ export class ProductosService {
 
   cargando = false;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
 
@@ -19,12 +20,50 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://bhinetportfolio.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
-        this.productos = resp;
-        this.cargando = false;
-        //console.log(resp);
-      });
+    return new Promise( ( resolve, reject ) => {
+
+      this.http.get('https://bhinetportfolio.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
+
+    });
+  }
+
+  getProducto( id: string ) {
+    return this.http.get(`https://bhinetportfolio.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0 ) {
+      this.cargarProductos().then( () => {
+        this.filtrarProductos( termino );
+      })
+    } else {
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const categoria = prod.categoria.toLocaleLowerCase();
+      const titulo = prod.titulo.toLocaleLowerCase();
+
+      if ( categoria.indexOf( termino ) >= 0 || titulo.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+    console.log(this.productosFiltrado);
 
   }
 }
